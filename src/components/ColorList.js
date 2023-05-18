5f5583b3396ebba32ee4a4ae955f3d4674fb77b0
vancouver29 <vancouver2990@gmailcom>
2023-05-18 16:29:53 +0200

    C6-React-context_Retrieving-colors-with-useContext

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,17 +1,31 @@
-import React from "react";
+// import React from "react";
+// import Color from "./Color";
+
+// export default function ColorList({ colors = [], onRemoveColor = f => f, onRateColor = f => f }) {
+//     if (!colors.length) return <div>No Colors Listed.</div>;
+//     return (
+//         <div>
+//             {colors.map(color =>
+//                 <Color key={color.id}
+//                     {...color}
+//                     onRemove={onRemoveColor}
+//                     onRate={onRateColor}
+//                 />)
+//             }
+//         </div>
+//     );
+// }
+
+import React, { useContext } from "react";
+import { ColorContext } from "./";
 import Color from "./Color";
 
-export default function ColorList({ colors = [], onRemoveColor = f => f, onRateColor = f => f }) {
+export default function ColorList() {
+    const { colors } = useContext(ColorContext);
     if (!colors.length) return <div>No Colors Listed.</div>;
     return (
-        <div>
-            {colors.map(color =>
-                <Color key={color.id}
-                    {...color}
-                    onRemove={onRemoveColor}
-                    onRate={onRateColor}
-                />)
-            }
+        <div className="color-list">
+            {colors.map(color => <Color key={color.id} {...color} />)}
         </div>
     );
 }
