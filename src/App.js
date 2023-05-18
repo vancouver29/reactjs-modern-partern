35da4ab89f7b3637421936b797ae90dba741b8e1
vancouver29 <vancouver2990@gmailcom>
2023-05-18 15:19:05 +0200

    C6-React-Context_Placing-colors-in-context

--- src/App.js
+++ src/App.js
@@ -1,56 +1,69 @@
-import React, { useState } from "react";
-import colorData from "./color-data.json";
+// import React, { useState } from "react";
+// import colorData from "./color-data.json";
+// import ColorList from "./components/ColorList";
+// import AddColorForm from "./components/AddColorForm";
+// import { v4 } from 'uuid';
+
+
+
+// function App() {
+//   const [colors, setColors] = useState(colorData);
+
+//   const removeColor = (id) => {
+//     const newColors = colors.filter(color => color.id !== id);
+//     setColors(newColors);
+//   }
+
+//   const rateColor = (id, rating) => {
+//     const newColors = colors.map(color => {
+//       if (color.id === id) {
+//         return {
+//           ...color,
+//           rating
+//         };
+//       }
+//       return color;
+//     });
+//     setColors(newColors);
+//   }
+
+//   return (
+//     <>
+//       <AddColorForm onNewColor={(title, color) => {
+//         const newColors = [...colors, {
+//           id: v4(),
+//           title,
+//           color,
+//           rating: 0
+//         }];
+//         setColors(newColors);
+//       }}
+//       />
+
+//       <ColorList
+//         colors={colors}
+//         onRemoveColor={removeColor}
+//         onRateColor={rateColor}
+//       />;
+
+//     </>
+//   )
+// }
+
+
+import React from "react";
 import ColorList from "./components/ColorList";
 import AddColorForm from "./components/AddColorForm";
-import { v4 } from 'uuid';
-
 
 
 function App() {
-  const [colors, setColors] = useState(colorData);
-
-  const removeColor = (id) => {
-    const newColors = colors.filter(color => color.id !== id);
-    setColors(newColors);
-  }
-
-  const rateColor = (id, rating) => {
-    const newColors = colors.map(color => {
-      if (color.id === id) {
-        return {
-          ...color,
-          rating
-        };
-      }
-      return color;
-    });
-    setColors(newColors);
-  }
 
   return (
     <>
-      <AddColorForm onNewColor={(title, color) => {
-        const newColors = [...colors, {
-          id: v4(),
-          title,
-          color,
-          rating: 0
-        }];
-        setColors(newColors);
-      }}
-      />
-
-      <ColorList
-        colors={colors}
-        onRemoveColor={removeColor}
-        onRateColor={rateColor}
-      />;
-
+      <AddColorForm />
+      <ColorList />;
     </>
   )
-
-
-
 }
 
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { createContext } from 'react';
+import App from './App';
+import colors from './color-data.json';
+import { render } from 'react-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+export const ColorContext = createContext();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ColorContext.Provider value={{ colors }}>
     <App />
-  </React.StrictMode>
+  </ColorContext.Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
